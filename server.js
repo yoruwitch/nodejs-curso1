cb2f5147feadf8fff0bc6e5f41a812ecefff5673
yoruwitch <evelyn.fernandes4@gmail.com>
2025-02-01 12:39:50 -0300

    feat: express for api and routes

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const routes = {
-    "/": "First route in Node.Js",
-    "/books": "Book's route",
-    "/authors": "Author's route",
-};
-
-const server = http.createServer((req, res) => {
-    //writeHead => para criar o cabeçalho da requisição
-    res.writeHead(200, { "Content-Type": "text/plain" });
-
-    // res.end(routes[req.url]); => req.url ->
-    res.end(routes[req.url]);
-});
-
 // 3000 é a porta a ser usado, depois precisa de uma arrow function
-server.listen(PORT, () => {
-    console.log("Servidor escutando");
+app.listen(PORT, () => {
+    console.log("Server is listening");
 });
