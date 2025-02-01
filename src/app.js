47beb2ae89518b1f958ee730b421e754738a1829
yoruwitch <evelyn.fernandes4@gmail.com>
2025-02-01 14:49:18 -0300

    feat: implement PUT and DELETE operations for books

--- src/app.js
+++ src/app.js
@@ -9,6 +9,13 @@ const books = [
     { id: 2, title: "1984" },
 ];
 
+function searchBook(id) {
+    return books.findIndex((book) => {
+        // é preciso converter o id para number para fazer a comparação
+        return book.id === Number(id);
+    });
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("First route in Node.Js");
 });
@@ -18,10 +25,28 @@ app.get("/books", (req, res) => {
     res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+    // params é parâmetro
+    const index = searchBook(req.params.id);
+    res.status(200).json(books[index]);
+});
+
 app.post("/books", (req, res) => {
     //req.body -> corpo da requisição, para usar um POST, precisamos usar o body para enviar como request
     books.push(req.body);
     res.status(201).send("Book has been added successfully");
+});
+
+app.put("/books/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    books[index].title = req.body.title;
+    res.status(200).json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    books.splice(index, 1);
+    res.status(200).send("Book has been deleted successfully");
 });
 
 export default app;
